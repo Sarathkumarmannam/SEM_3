000ffd59c9c3602644fc620ae6f83764f114cd45
Sarath Kumar Mannam <sk.mannam@gmail.com>
2018-07-12 07:51:33 +0000

    circular list

--- DS/CircularList.c
+++ DS/CircularList.c
@@ -1,4 +1,6 @@
 //A program on circular linkedlist
+//deleting an element from circular list
+//Searching and sorting in a cirular list
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct CList{
@@ -6,14 +8,17 @@ typedef struct CList{
     struct CList *next;
 }node;
 node *head;
+void Delete();
 void createList();
 void display();
+void AddNode();
+void Search();
 int main()
 {
     int ch;
-    printf("PRESS \n 1->create Circular Linked list\n2->Display\n");
     do{
-        printf("Enter your choice\n");
+        printf("PRESS \n 1->create Circular Linked list\n2->Display\n");
+        printf("3->NewNode\n4->deleteNode\n5.Search\nEnter your choice\n");
         scanf("%d",&ch);
         switch(ch){
             case 1:
@@ -22,22 +27,73 @@ int main()
             case 2:
                 display();
                 break;
-            /*case 3:
+            case 3:
                 AddNode();
                 break;
             case 4:
-                DeleteNode();
-                break;*/
+                Delete();
+                break;
+            case 5:
+                Search();
+                break;
             default:
                 {
                     printf("Enter choice in selected series\n");
                     printf("Do you still wanna continue(1|0)");
                     scanf("%d",&ch);
                 }
+                 }
     }
-    while(ch>=1&&ch<=4);
+    while(ch>=1&&ch<=5);
+}
+void Search(){
+    node *temp=head;
+    int n;
+    printf("Enter data of node to search\n");
+    scanf("%d",&n);
+    do
+    {
+        if(temp->data==n){
+            n=0;break;}
+        else{
+            // go to next address
+            temp=temp->next;
+        }
+    }
+    while(temp!=head);
+    if(n==0)
+    printf("Required data is found ^_^\n");
+    else
+    printf("Required data is not found\n");
 
-
+}
+void Delete(){
+    node *temp=head;
+    printf("Enter node number to delete\n");
+    int pos,count=1;
+    scanf("%d",&pos);
+    while(pos!=(count))
+    {
+        if(count==1)     {count++;continue;}
+        temp=temp->next;
+        count++;
+    }
+    node *ptr=temp->next->next;
+    free(temp->next);
+    temp->next=ptr;
+    printf("Node at position %d is successfully deleted\n",pos);
+}
+void AddNode(){
+    node* OneMore=(node*)malloc(sizeof(node));
+    node *temp=head;
+    while(temp->next!=head)
+        temp=temp->next;
+    printf("Enter Data for NewNode\t");
+    scanf("%d",&OneMore->data);
+    temp->next=OneMore;
+    OneMore->next=head;
+    temp=head;
+    printf("New Node is successfully created^_^\n");
 }
 void createList(){
     head=(node*)malloc(sizeof(node));
@@ -48,7 +104,7 @@ void createList(){
     do{
         printf("Enter data of the node\n");
         scanf("%d",&temp->data);
-         printf("1 to Continue|0 to drop:\t");
+        printf("1 to Continue|0 to drop:\t");
         scanf("%d",&flag);
         temp->next=head;
         if(flag==0)
@@ -70,7 +126,7 @@ printf("\n---^_^--Circular List Successfully created----^_^----\n");
 void display()
 {
     node *temp=head;
-    printf("Node 1 data is %d",temp->data);
+    printf("Node 1 data is %d\n",temp->data);
     temp=temp->next;
     int i=2;
     while(temp!=head)
